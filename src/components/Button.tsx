1909649d91865726d25bf77cdf16a734e1aa97e1
oliversaternus <mail@oliversaternus.de>
2019-09-21 12:45:36 +0200

    animejs

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,19 +1,44 @@
 import * as React from "react";
 import { observer, inject } from "mobx-react";
 import SizeStore from "../stores/SizeStore";
+import { Button, Theme, withStyles } from "@material-ui/core";
+import { createStyles } from "@material-ui/styles";
 
 interface ButtonProps {
   name: string;
   color?: string;
   sizeStore?: SizeStore;
-}
-
-@inject("sizeStore")
-@observer
-class Button extends React.Component<ButtonProps, {}> {
-  render() {
-    return <button>Hello, {this.props.sizeStore.width}</button>;
+  classes: {
+    root: string;
   }
 }
 
-export default Button;
+const styles = ({ palette, spacing }: Theme) => createStyles({
+  root: {
+    display: 'flex',
+    width: '100%',
+    flexDirection: 'column',
+    alignItems: 'center',
+    justifyContent: 'center',
+    padding: spacing,
+    backgroundColor: palette.background.default,
+    color: palette.primary.main,
+  },
+});
+
+@inject("sizeStore")
+@observer
+class Page extends React.Component<ButtonProps, {}> {
+  render() {
+    const { classes } = this.props;
+    return (
+      <div className={classes.root}>
+        <Button variant="contained" color="primary" >
+          Hello
+      </Button>
+      </div>
+    );
+  }
+}
+
+export default withStyles(styles as any)(Page);
--- src/index.tsx
+++ src/index.tsx
@@ -3,16 +3,38 @@ import * as ReactDOM from "react-dom";
 import { Provider } from "mobx-react";
 import SizeStore from "./stores/SizeStore";
 import { HashRouter } from 'react-router-dom';
+import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
 import App from './App';
 
 const sizeStore = new SizeStore();
+
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: '#155799',
+      contrastText: '#ffffff',
+    },
+    secondary: {
+      main: '#ebeef0',
+      contrastText: '#000000',
+    },
+    action: {
+      disabledBackground: '#09421d'
+    },
+    background: {
+      default: '#2f2f33'
+    }
+  },
+});
 
 ReactDOM.render(
   <Provider
     sizeStore={sizeStore}
   >
     <HashRouter>
-      <App />
+      <MuiThemeProvider theme={theme}>
+        <App />
+      </MuiThemeProvider>
     </HashRouter>
   </Provider>,
   document.getElementById("root")
